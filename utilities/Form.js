86f71d4e626425750b150dc3b44e01f68085a004
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-22 01:54:51 +0530

    Finalised Vault Form

---
+++ utilities/Form.js
@@ -0,0 +1,36 @@
+import dayjs from 'dayjs'
+
+export function reduceErrors(errors) {
+    const messages = []
+    for (const key in errors) {
+        const value = errors[key]
+        if (typeof value === 'string') {
+            messages.push(value)
+        } else if (typeof value === 'object') {
+            messages.push(...reduceErrors(value))
+        } else if (Array.isArray(value)) {
+            for (const item of value) {
+                messages.push(...reduceErrors(item))
+            }
+        }
+    }
+    return [...new Set(messages)]
+}
+
+export function calcTotal(items) {
+    let total = 0
+    for (const item of items) {
+        total += item.total
+    }
+    return total
+}
+
+export function createVault(status, values) {
+    return ({
+        ...values,
+        createdAt: dayjs(values.createdAt).format('YYYY-MM-DD'),
+        paymentDue: dayjs(values.createdAt).add(Number(values.paymentTerms), 'day').format('YYYY-MM-DD'),
+        status,
+        total: calcTotal(values.items)
+    })
+}
