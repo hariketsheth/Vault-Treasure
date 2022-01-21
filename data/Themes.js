148a1a6fee299928ea58afd53bf497a407ed4d95
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-22 01:53:33 +0530

    Added Theme

---
+++ data/Themes.js
@@ -0,0 +1,133 @@
+export const light = {
+	color: {
+		body: {
+			bg: '#f8f8fb'
+		},
+		sidebar: {
+			bg: '#373b53'
+		},
+		form: {
+			bg: '#ffffff',
+			fieldBg: '#ffffff',
+			fieldBorder: '#DFE3FA'
+		},
+		btn: {
+			secondary: {
+				bg: '#f9fafe',
+				hover: '#dfe3fa',
+				text: '#7e88c3'
+			},
+			tertiary: {
+				bg: '#363b53',
+				hover: '#0c0e16',
+				text: '#888eb0'
+			},
+			quaternary: {
+				bg: '#F9FAFE',
+				hover: '#DFE3FA',
+				text: '#7E88C3'
+			}
+		},
+		VaultItem: {
+			bg: '#ffffff'
+		},
+		Vaultstatus: {
+			bg: 'rgba(55, 59, 83, 0.06)',
+			text: '#373B53'
+		},
+		VaultTable: {
+			bg: '#f9fafe',
+			footerBg: '#373b53'
+		},
+		popup: {
+			bg: '#ffffff'
+		},
+		dropdown: {
+			bg: '#ffffff',
+			shadow: 'rgba(72, 84, 159, 0.25)'
+		},
+		checkbox: {
+			bg: '#dfe3fa'
+		},
+		text: {
+			heading: '#0C0E16',
+			bodyA: '#888eb0',
+			bodyB: '#858BB2',
+			formLabel: '#7E88C3',
+			link: '#0c0e16',
+			linkHover: '#7e88c3',
+			placeholder: 'rgba(12, 14, 22, .4)'
+		}
+	},
+	icon: {
+		path: '/images/icon-moon.svg',
+		alt: 'moon icon'
+	}
+}
+
+export const dark = {
+	color: {
+		body: {
+			bg: '#141625'
+		},
+		sidebar: {
+			bg: '#1e2139'
+		},
+		form: {
+			bg: '#141625',
+			fieldBg: '#1e2139',
+			fieldBorder: '#252945'
+		},
+		btn: {
+			secondary: {
+				bg: '#252945',
+				hover: '#ffffff',
+				text: '#dfe3fa'
+			},
+			tertiary: {
+				bg: '#363b53',
+				hover: '#1e2139',
+				text: '#dfe3fa'
+			},
+			quaternary: {
+				bg: '#252945',
+				hover: '#1e2139',
+				text: '#DFE3FA'
+			}
+		},
+		VaultItem: {
+			bg: '#1e2139'
+		},
+		Vaultstatus: {
+			bg: 'rgba(223, 227, 250, .06)',
+			text: '#dfe3fa'
+		},
+		VaultTable: {
+			bg: '#252945',
+			footerBg: '#0C0e16'
+		},
+		popup: {
+			bg: '#1e2139'
+		},
+		dropdown: {
+			bg: '#252945',
+			shadow: 'rgba(0, 0, 0, 0.25)'
+		},
+		checkbox: {
+			bg: '#1e2139'
+		},
+		text: {
+			heading: '#ffffff',
+			bodyA: '#dfe3Fa',
+			bodyB: '#ffffff',
+			formLabel: '#dfe3fa',
+			link: '#ffffff',
+			linkHover: '#888eb0',
+			placeholder: '#fff'
+		}
+	},
+	icon: {
+		path: '/images/icon-sun.svg',
+		alt: 'sun icon'
+	}
+}
