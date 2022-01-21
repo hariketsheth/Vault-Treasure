ee57472535d8a79145002092df82ba1a644d227e
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-22 02:15:42 +0530

    Added VaultFooter

---
+++ components/Vault/VaultFooter.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
+
+import Button from '../shared/Buttons'
+
+const Wrapper = styled(motion.div)`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: .5rem;
+    width: 100%;
+    padding: 1.5rem;
+    background: ${props => props.theme.color.VaultItem.bg};
+    transition: background .3s;
+
+    @media only screen and (min-width: 650px) {
+        display: none;
+    }
+`
+
+export default function VaultFooter({ status, setPopupIsOpen, setFormIsOpen, handlePaid }) {
+    return (
+        <Wrapper initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+            <Button secondary onClick={() => setFormIsOpen(true)}>Edit</Button>
+            <Button alert onClick={() => setPopupIsOpen(true)}>Delete</Button>
+            {status !== 'paid' ? <Button onClick={handlePaid}>Mark As Paid</Button> : null}
+        </Wrapper>
+    )
+}
