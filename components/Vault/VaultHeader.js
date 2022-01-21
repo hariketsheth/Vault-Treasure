123ef1575ed29f0346f49f9c39f5dde2fbdc28cf
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-22 02:00:11 +0530

    Finalised Header

---
+++ components/Vault/VaultHeader.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+import Vaultstatus from '../shared/Vaultstatus'
+import Button from '../shared/Buttons'
+import { fontStylesA } from '../shared/Typography'
+
+const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    border-radius: 8px;
+    box-shadow: 0px 10px 10px -10px rgba(72, 84, 159, 0.100397);
+    padding: 1.5rem;
+    background: ${props => props.theme.color.VaultItem.bg};
+    transition: background .3s;
+`
+
+const Status = styled.span`
+    ${fontStylesA}
+    margin-right: auto;
+
+    @media only screen and (min-width: 650px) {
+        margin-right: 1rem;
+    }
+`
+
+const Buttons = styled.div`
+    display: none;
+
+    @media only screen and (min-width: 650px) {
+        display: flex;
+        gap: .5rem;
+        margin-left: auto;
+    }
+`
+
+export default function VaultHeader({ className, status, setPopupIsOpen, setFormIsOpen, handlePaid }) {
+    return (
+        <Wrapper className={className}>
+            <Status>Status</Status>
+            <Vaultstatus status={status}/>
+            <Buttons>
+                <Button secondary onClick={() => setFormIsOpen(true)}>Edit</Button>
+                <Button alert onClick={() => setPopupIsOpen(true)}>Delete</Button>
+                {status !== 'paid' ? <Button onClick={handlePaid}>Mark As Paid</Button> : null}
+            </Buttons>
+        </Wrapper>
+    )
+}
