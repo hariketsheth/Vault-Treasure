d912e7c8de8cf8eecc3951171f12a9683c538687
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-22 02:14:19 +0530

    Added Header.js

---
+++ components/home/Header.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
+
+import Dropdown from './Dropdown'
+import { Heading1 } from '../shared/Headings'
+import { fontStylesA } from '../shared/Typography'
+import { ButtonPlus } from '../shared/Buttons'
+
+import { VaultsMessage } from '../../utilities/Misc'
+
+const Wrapper = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 4rem;
+`
+
+const TextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-right: auto;
+`
+
+const Heading = styled(Heading1)`
+    margin-bottom: .5rem;
+    font-size: 1.25rem;
+
+    @media only screen and (min-width: 550px) {
+        font-size: 2rem;
+    }
+`
+
+const Subheading = styled.div`
+    ${fontStylesA}
+`
+
+const Button = styled(ButtonPlus)`
+    margin-left: 1rem;
+
+    .btn-plus-Vault {
+        display: none;
+    }
+
+    @media only screen and (min-width: 550px) {
+        margin-left: 2.5rem;
+
+        .btn-plus-Vault {
+            display: initial;
+        }
+    }
+`
+
+const animation = {
+    hidden: { 
+        opacity: 0,
+        transition: { duration: 1 }
+    },
+    visible: { 
+        opacity: 1,
+        transition: { duration: 1 }
+    }
+}
+
+export default function Header({ Vaults, filter, setFilter, setFormIsOpen }) {
+    const message = VaultsMessage(Vaults && Vaults.length, filter)
+
+    return (
+        <Wrapper
+            variants={animation}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+        >
+            <TextWrapper>
+                <Heading>Vaults</Heading>
+                <Subheading>{message}</Subheading>
+            </TextWrapper>
+            <Dropdown setFilter={setFilter}/>
+            <Button onClick={() => setFormIsOpen(true)}>New <span className="btn-plus-Vault">Vault</span></Button>
+        </Wrapper>
+    )
+}
